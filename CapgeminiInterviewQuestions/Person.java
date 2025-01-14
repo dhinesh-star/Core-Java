3da0c74f46b6b477c81e783933cfb0c00ce4ee1d
Dhinesh <dhineshkumaran2001@gmail.com>
2025-01-15 02:50:54 +0530

    Capgemini Interview Question -> Using StreamAPI

---
+++ CapgeminiInterviewQuestions/Person.java
@@ -0,0 +1,37 @@
+package CapgeminiInterviewQuestions;
+
+public class Person {
+    private String name;
+    private Integer age;
+    private Integer salary;
+
+    public Person(String name, Integer salary, Integer age) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
---
+++ CapgeminiInterviewQuestions/StreamQuestion.java
@@ -0,0 +1,26 @@
+package CapgeminiInterviewQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+    Declare a Person Class with following variables
+    name, age, salary.
+    1. Get the name olded person(based on age) from the List of Person variables
+*/
+public class StreamQuestion {
+    public static void main(String[] args) {
+        List<Person> personList = Arrays.asList(
+            new Person("abcd", 50000, 23),
+            new Person("efgh", 150000, 28),
+            new Person("ijkl", 100000, 30)
+        );
+
+        Person maxAgePerson = personList.stream()
+                .reduce(personList.get(0), (p1, p2) ->{
+                    if(p1 != null && p1.getAge() > p2.getAge()) return p1;
+                    return p2;
+                });
+         System.out.println("maxAgePerson name >>"+maxAgePerson.getName());
+    }
+}
